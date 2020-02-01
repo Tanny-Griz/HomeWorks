ae77275920d6e0407b9f7e6fc19bfc68bf0e4833
Tanya Kovalenko <tanya.griz89@gmail.com>
2020-02-01 16:12:56 +0200

    HW15 apdate

--- HW13-15(Pizza)/main.js
+++ HW13-15(Pizza)/main.js
@@ -1,6 +1,12 @@
+window.cElem = (tagName, className = "") => {
+    const element = document.createElement(tagName)
+    element.className = className;
+    return element
+}
+
 // ----- modal-----
 const pizzaCardContainer = document.querySelector('.pizza-info');
-const modal = document.getElementById('modal-content');
+const modal = document.getElementById('pizzaInfo');
 
 // ф-я закрытия модалки
 function hendlerClose(e) {
@@ -59,8 +65,8 @@ const modalCreate = document.getElementById('modal-content-create');
 createPizza.addEventListener('click', hendlerClose);
 
 const renderMyPizzaCreateModal = () => {
-    const pizzaInfoCard = document.createElement('div')
-    pizzaInfoCard.className = 'pizza-info__card';
+    const pizzaInfoCard = cElem('div', 'pizza-info__card');
+
 
     const pizzaInfoHeader = document.createElement('div');
     pizzaInfoHeader.className = 'pizza-info__header';
@@ -68,6 +74,7 @@ const renderMyPizzaCreateModal = () => {
     const aClose =  document.createElement('a');
     aClose.className = 'pizza-info__close';
     aClose.innerText = 'X';
+    aClose.href = '#'
 
     let h2 = document.createElement('h2');
 
@@ -163,119 +170,117 @@ const renderСompositionList = (arrayOfСomposition) => {
 const createPizzaBtn = document.getElementById('create-pizza-btn');
 createPizzaBtn.onclick = () => renderСompositionList(compositionList);
 
-// ---- card ---- создаем карточку товара
+
+// СОЗДАЕМ КАРТОЧКУ ТОВАРА
 const renderCard = (pizza) => {
-    const holdCard = document.createElement('div');
-    holdCard.className = 'hold-card';
-    const card = document.createElement('div');
-    card.className = 'card';
+    const holdCard = cElem('div', 'hold-card');
+
+    const card = cElem('div', 'card');
     card.id = `pizza${pizza.id}`;
+    // ПОКАЗ МОДАЛЬНОГО ОКНА
     card.onclick = function() {
         renderPizzaCard(pizza);
         pizzaCardContainer.style.display = 'flex';
     }
     holdCard.appendChild(card);
     // img
-    const img = document.createElement('div');
+    const img = cElem('div', 'card-img-top');
     img.innerHTML = `<img src="img/${pizza.img}" alt="icon">`;
-    img.classList.add('card-img-top');
     card.appendChild(img);
-    // 4. favorite
-    const pizzaFavorite = document.createElement('div');
-    pizzaFavorite.className = 'pizza-card__favor';
+    // В ИЗБРАННОЕ
+    const pizzaFavorite = cElem('div', 'pizza-card__favor');
     pizzaFavorite.innerHTML = `<button class="addToFavor"><span class="heart">&#10084;</span></button>`;
     pizzaFavorite.onclick = function () {
         pizza.isFavourite = true;
-        console.log(pizza);
     }
     card.appendChild(pizzaFavorite);
     // h3
-    const pizzaName = document.createElement('div');
-    pizzaName.className = 'pizza-card__name';
+    const pizzaName = cElem('div', 'pizza-card__name');
     pizzaName.innerHTML = `<h3>${pizza.name}</h3>`;
     card.appendChild(pizzaName);
     // p composition
-    const composition = document.createElement('div');
+    const composition = cElem('div', 'pizza-card__composition');
     composition.innerHTML = '<ul>' + 'Состав: ' + pizza.composition.map(c => `<li>${c},</li>`).join(' ') + '</ul>';
-    composition.className = 'pizza-card__composition';
     card.appendChild(composition);
     // p caloricity
-    const caloricity = document.createElement('p');
-    caloricity.className = 'pizza-card__caloricity';
+    const caloricity = cElem('p', 'pizza-card__caloricity');
     caloricity.innerText = `Ккал: ${pizza.caloricity}`;
     card.appendChild(caloricity);
     // p price
-    const price = document.createElement('p');
-    price.className = 'pizza-card__price';
+    const price = cElem('p', 'pizza-card__price');
     price.innerText = `Цена: ${pizza.price} грн.`;
     card.appendChild(price);
     // button
-    const button = document.createElement('button')
-    button.className = 'pizza-card__button';
+    const button = cElem('button', 'pizza-card__button')
     button.innerText = 'Заказать';
     card.appendChild(button);
-
     button.onclick = function (e) {
         console.log(this);
     }
     return holdCard;
 }
 
-// ---- end card ----
+// ---- end КАРТОЧКА ТОВАРА ----
 
-// рендерим в див
+
+// ОТРИСОВКА НА СТРАНИЦУ
+// создаем копию массива
+let newArrPizzaList = [...pizzaList];
+
+// куда отрисуем
+const mainElement = document.querySelector('.holder-pizzas-list');
+
 const renderHolderPizzasList = (arrayOfPizza) => {
-    const mainElement = document.querySelector('.holder-pizzas-list');
-
     mainElement.innerHTML = ''
     for (let pizza of arrayOfPizza) {
+        // каждая пицца залетает в renderCard(pizza)
         const card = renderCard(pizza);
         mainElement.appendChild(card);
     }
 }
-renderHolderPizzasList(pizzaList)
+renderHolderPizzasList(newArrPizzaList)
 
-// селект по возрастанию, по убыванию цены
+
+// СЕЛЕКТ по возрастанию, по убыванию
 const select = document.getElementById('select');
 
 select.onchange = function () {
-    const newArr = [...pizzaList];
-    newArr.sort((a, b) => {
+    newArrPizzaList.sort((a, b) => {
         if (a.price < b.price) return this.value === '1' ? -1 : 1
         if (a.price > b.price) return this.value === '1' ? 1 : -1
         if (a.price === b.price) return 0
     })
-    renderHolderPizzasList(newArr);
+    renderHolderPizzasList(newArrPizzaList);
 }
 
-// 1. Поиск должен быть живой, то есть при вводе символов в input без нажатия на кнопку search должны выводиться соответсвующие карточки.
 
+// ЖИВОЙ ПОИСК
 const input = document.getElementById('inputFind');
 
 input.addEventListener('input', function (e) {
-    let newArr = [...pizzaList].filter(pizza => {
+    let newArr = newArrPizzaList.filter(pizza => {
         let eTargetValue = e.target.value.toLowerCase();
-        if (pizza.name.toLowerCase().includes(eTargetValue)) {
-            return true
-        }
-        if (pizza.composition.join(' ').toLowerCase().includes(eTargetValue)) {
-            return true
-        }
-    })
+        // если совпадения есть в именах или составе
+        let pizzaNameIncludes = pizza.name.toLowerCase().includes(eTargetValue);
+        let pizzaCompositionIncludes = pizza.composition.join(' ').toLowerCase().includes(eTargetValue);
+        // вернуть по пиццу с таким именем либо составом (избежать дублирования)
+        return pizzaNameIncludes || pizzaCompositionIncludes
+    });
     newArr = newArr.map(pizza => {
-        let composition = [...pizza.composition].map(comp => {
+        let composition = pizza.composition.map(comp => {
             if (e.target.value.length && comp.includes(e.target.value)) {
-                return `<span class="pink">${comp}</span>`
+                return `<span class="pink">${comp}</span>`;
             }
             return comp
         })
         return {...pizza, composition}
     })
-    renderHolderPizzasList(newArr)
+    renderHolderPizzasList(newArr);
 })
+// ----------------------------------------
 
-// 2. Добавить фильтрацию по-калориям и по-цене. Фильтрация должна иметь поля ( input ) "от" и "до". Выводиться новый пиццы должны только при нажатии на кнопку search.
 
+// ФИЛЬТР по каллориям или цене (От - До)
 let inputFrom = document.getElementById('inputFrom');
 let inputTo = document.getElementById('inputTo');
 let btnFind = document.getElementById('btn-find');
@@ -284,7 +289,7 @@ const selectFilter = document.getElementById('select-filter');
 let sortPizzasByCaloricity = () => {
     const valueOfFilterFrom = inputFrom.value
     const valueOfFilterTo = inputTo.value
-    const newArrCaloricity = [...pizzaList].filter((pizza) => {
+    const newArrCaloricity = newArrPizzaList.filter((pizza) => {
         if (pizza.caloricity > valueOfFilterFrom && pizza.caloricity < valueOfFilterTo) {
             return true
         }
@@ -296,7 +301,7 @@ let sortPizzasByCaloricity = () => {
 let sortPizzasByPrice = () => {
     const valueOfFilterFrom = inputFrom.value
     const valueOfFilterTo = inputTo.value
-    const newArrPrice = [...pizzaList].filter((pizza) => {
+    const newArrPrice = newArrPizzaList.filter((pizza) => {
         if (pizza.price > valueOfFilterFrom && pizza.price < valueOfFilterTo) {
             return true
         }
@@ -304,6 +309,8 @@ let sortPizzasByPrice = () => {
     })
     renderHolderPizzasList(newArrPrice);
 }
+
+// кнопка ПРИМЕНИТЬ ФИЛЬТР
 btnFind.onclick = function () {
     if (selectFilter.value == '1') {
         sortPizzasByCaloricity();
@@ -312,8 +319,10 @@ btnFind.onclick = function () {
         sortPizzasByPrice();
     }
 }
+//------------------------------------------------------
 
-// 3. Добавить кнопку сброса фильтров.
+
+// КНОПКА СБРОСА ФИЛЬТРОВ
 let btnReset = document.getElementById('btn-reset');
 
 btnReset.addEventListener('click', resetFilterValue);
@@ -321,33 +330,28 @@ btnReset.addEventListener('click', resetFilterValue);
 function resetFilterValue() {
     inputFrom.value = null;
     inputTo.value = null;
-    renderHolderPizzasList(pizzaList);
+    renderHolderPizzasList(newArrPizzaList);
 }
 
-// 4. Добавить возможность добовлять пиццу в избранное. То есть при нажатии на кнопку, которая находится на карточке с пиццей в виде сердечка или любого другого элемента пицца принимает поле isFavourite: true.
 
+// ДОБАВИТЬ В ИЗБРАННОЕ
 const addToFavor = document.getElementById('addToFavor');
 
-// 5. Добавить баннер - слайдер, где автоматически будут каждую секунду показываться пицца с полем priceOfTheDay: true, которое вы можете сами добавить любым на ваш быбор пиццам в самом коде. Можете имспользовать мой обновленный массив пицц. Банер должен распологаться выше карточек с пиццами. Дизайн произвольный. Разрешено использовать в качесиве слайдера любую библиотеку.
 
+// БАННЕР / СЛАЙДЕР
 const renderSlide = (pizza) => {
-    const holderSlider = document.createElement('div');
-    holderSlider.className = 'row holder-slider-item text-center';
+    const holderSlider = cElem('div', 'row holder-slider-item text-center');
     // img
-    const slideImg = document.createElement('div');
-    slideImg.className = 'col-sm-12 col-md-6 slide-img';
-    const img = document.createElement('img');
+    const slideImg = cElem('div', 'col-sm-12 col-md-6 slide-img');
+    const img = cElem('img', 'simg');
     img.alt = 'icon';
-    img.classList.add('simg');
     img.src = 'img/' + pizza.img;
     // text
-    const slideText = document.createElement('div');
-    slideText.className = 'col-sm-12 col-md-6 slide-text';
+    const slideText = document.createElement('div', 'col-sm-12 col-md-6 slide-text');
     slideText.innerHTML = `
                         <p>Предложение дня!</p>
                         <p>3 по цене 2</p>
                         `;
-    // в див slide-img засунем img
     slideImg.appendChild(img);
     holderSlider.appendChild(slideImg);
     holderSlider.appendChild(slideText);
@@ -355,18 +359,16 @@ const renderSlide = (pizza) => {
     return holderSlider
 }
 
-let indexOfName = 0;
 
-// массив с отфильтрованными пиццами по priceOfTheDay
-const pizzaOfTheDay = pizzaList.filter(pizza => {
+// ПИЦЦЫ из предложения дня
+let indexOfName = 0;
+const pizzaOfTheDay = newArrPizzaList.filter(pizza => {
     if (pizza.priceOfTheDay == true) {
         return true
     }
 })
-// console.log(pizzaOfTheDay);
 
-
-// заходит первая пицца
+// отрисовка в БАННЕР Акционных пиц
 const renderSlideContainer = (pizza) => {
     const sliderContainer = document.querySelector('.slider');
     sliderContainer.innerHTML = '';
@@ -375,16 +377,11 @@ const renderSlideContainer = (pizza) => {
         sliderContainer.appendChild(slide);
     }
 }
-
 renderSlideContainer(pizzaOfTheDay[indexOfName]); // отрисовываем первую, без интервала
 
 setInterval(() => {
     // если индекс = последнему (концу) массива, то индексу снова присваиваем 0, иначе +1
     indexOfName = indexOfName === pizzaOfTheDay.length - 1 ? 0 : indexOfName + 1
     renderSlideContainer(pizzaOfTheDay[indexOfName]); // через каждые 2 сек отрысов по очереди 
-}, 2000);
-
-
-// Создание новой пиццы
-console.log(compositionList)
+}, 3000);
 
